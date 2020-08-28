5ab8d3cf652cc102b0a3d5dada97cbf0e76bfab9
AMoutousis <38360122+Swoopsy@users.noreply.github.com>
2020-08-28 12:25:07 -0400

    First commit for todo list

---
+++ ToDoList/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class HomeController : Controller
+    {
+        //this line is a pointer to database
+        private readonly ToDoListIdentityDbContext _todolistdb;
+
+        public HomeController(ToDoListIdentityDbContext todolistdb)
+        {
+            _todolistdb = todolistdb;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Privacy()
+        {
+            //getting the id of the user currently logged in
+            string id = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            //get a list of tasks assigned to the person
+            var todoList = _todolistdb.ToDoList.Where(x => x.UserId == id).ToList();
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ToDoList/Models/ToDoList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDoList.Models
+{
+    public partial class ToDoList
+    {
+        public int Id { get; set; }
+        public string ToDoName { get; set; }
+        public string ToDoDescription { get; set; }
+        public DateTime? DueDate { get; set; }
+        public bool Complete { get; set; }
+        public string UserId { get; set; }
+
+        public virtual AspNetUsers User { get; set; }
+    }
+}
